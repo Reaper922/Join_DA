e99b44e05193546b385e2f8d59a1e54b0c535943
Dennis Ammen <dennis.ammen@outlook.com>
2023-02-18 10:25:23 +0100

    Split up script.js

---
+++ js/login.js
@@ -0,0 +1,22 @@
+function buttonEventListener() {
+    const signupBtn = document.getElementById('signup');
+    const loginBtn = document.getElementById('login');
+    const guestLoginBtn = document.getElementById('guest-login');
+
+    signupBtn.addEventListener('click', () => {
+        window.location.href = 'sign-up.html';
+    });
+
+    loginBtn.addEventListener('click', e => {
+        e.preventDefault();
+        window.location.href = 'summary.html';
+    });
+
+    guestLoginBtn.addEventListener('click', e => {
+        e.preventDefault();
+        window.location.href = 'summary.html';
+    });
+}
+
+
+if (location.pathname === '/index.html') { buttonEventListener() }
--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 'use strict';
 
-window.onload = () => {
-    includeHTML();
-    if (window.location.pathname === '/index.html') { buttonEventListener() }
+
+/**
+ * Initial function that gets executed after the document is loaded.
+ */
+async function init() {
+    await includeHTML();
+    highlightActiveMenuItem();
+    logoutModalEventListener();
 }
 
 
+/**
+ * Inserts the HTML from the template files into the referenced file.
+ */
 async function includeHTML() {
     const includeElements = document.querySelectorAll('[template-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -21,22 +29,59 @@ async function includeHTML() {
 }
 
 
-function buttonEventListener() {
-    const signupBtn = document.getElementById('signup');
-    const loginBtn = document.getElementById('login');
-    const guestLoginBtn = document.getElementById('guest-login');
+/**
+ * Highlights the active menu item after the page is loaded. The marking takes place on the basis of the path.
+ */
+function highlightActiveMenuItem() {
+    const currentPath = location.pathname;
+    const legalEl = document.getElementById('legal-desktop');
 
-    signupBtn.addEventListener('click', () => {
-        window.location.href = 'sign-up.html';
-    });
+    switch (currentPath) {
+        case '/summary.html':
+            addActiveClass('summary');
+            break;
+        case '/board.html':
+            addActiveClass('board');
+            break;
+        case '/task.html':
+            addActiveClass('task');
+            break;
+        case '/contacts.html':
+            addActiveClass('contacts');
+            break;
+        case '/legal.html':
+            legalEl.classList.add('active');
+            break;
+        default:
+            break;
+    }
+}
 
-    loginBtn.addEventListener('click', e => {
-        e.preventDefault();
-        window.location.href = 'summary.html';
-    });
 
-    guestLoginBtn.addEventListener('click', e => {
-        e.preventDefault();
-        window.location.href = 'summary.html';
+/**
+ * Adds the class .active to the given element.
+ * @param {String} item String of the item
+ */
+function addActiveClass(item) {
+    const desktopEl = document.getElementById(`${item}-desktop`);
+    const mobileEl = document.getElementById(`${item}-mobile`);
+
+    desktopEl.classList.add('active');
+    mobileEl.classList.add('active');
+}
+
+
+/**
+ * Adds an event listener to the profile picture to toggle the logout modal.
+ */
+function logoutModalEventListener() {
+    const profilePicture = document.getElementById('profile-picture');
+    const logoutModal = document.getElementById('logout-modal');
+
+    profilePicture.addEventListener('click', () => {
+        logoutModal.classList.toggle('d-none');
     });
 }
+
+
+init();
