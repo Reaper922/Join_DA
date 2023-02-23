08486c86f277381e70394af5722c0e6724c637b4
Dennis Ammen <dennis.ammen@outlook.com>
2023-02-23 14:34:14 +0100

    Added ts-check

--- js/login.js
+++ js/login.js
@@ -1,18 +1,20 @@
+// @ts-check
+
 function buttonEventListener() {
     const signupBtn = document.getElementById('signup');
     const loginBtn = document.getElementById('login');
     const guestLoginBtn = document.getElementById('guest-login');
 
-    signupBtn.addEventListener('click', () => {
+    signupBtn?.addEventListener('click', () => {
         window.location.href = 'sign-up.html';
     });
 
-    loginBtn.addEventListener('click', e => {
+    loginBtn?.addEventListener('click', e => {
         e.preventDefault();
         window.location.href = 'summary.html';
     });
 
-    guestLoginBtn.addEventListener('click', e => {
+    guestLoginBtn?.addEventListener('click', e => {
         e.preventDefault();
         window.location.href = 'summary.html';
     });
